5aa07841ed17305a363ae8402c92641f49d4cb56
Joe Guilmette <joe.guilmette@gmail.com>
2014-10-16 14:04:33 +0700

    add hhvvvm support for nginx.conf, add wordmove, sass and compass to prov-pre

---
+++ provision/provision-pre.sh
@@ -0,0 +1,63 @@
+# Rubygems update
+
+if [ $(gem -v|grep '^2.') ]; then
+	echo "gem installed"
+else
+	apt-get install -y ruby-dev
+	echo "ruby-dev installed"
+	echo "gem not installed"
+	gem install rubygems-update
+	update_rubygems
+fi
+
+# wordmove install
+wordmove_install="$(gem list wordmove -i)"
+if [ "$wordmove_install" = true ]; then
+  echo "wordmove installed"
+else
+  echo "wordmove not installed"
+  gem install wordmove
+
+  wordmove_path="$(gem which wordmove | sed -s 's/.rb/\/deployer\/base.rb/')"
+  if [  "$(grep yaml $wordmove_path)" ]; then
+
+
+    echo "can require yaml"
+  else
+    echo "can't require yaml"
+    echo "set require yaml"
+
+    sed -i "7i require\ \'yaml\'" $wordmove_path
+
+    echo "can require yaml"
+
+  fi
+fi
+
+#sass install
+sass_install="$(gem list sass -i)"
+if [ "$sass_install" = true ]; then
+	echo "sass installed"
+else
+	echo "sass not installed"
+	gem install sass
+fi
+
+#compass install
+compass_install="$(gem list compass -i)"
+if [ "$compass_install" = true ]; then
+	echo "compass installed"
+else
+	echo "compass not installed"
+	gem install compass --pre
+fi
+
+#compass-rgbapng install
+#currently only needed for MyLooks project
+compass-rgbapng_install="$(gem list compass-rgbapng -i)"
+if [ "$compass-rgbapng_install" = true ]; then
+  echo "compass-rgbapng installed"
+else
+  echo "compass-rgbapng not installed"
+  gem install compass-rgbapng
+fi
